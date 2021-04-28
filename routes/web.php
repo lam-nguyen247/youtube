0a2db5d3fb4eb3df0f0ba84900de59d7d2c3ad0e
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-04-28 10:40:15 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('{digitalMarketing}', [DigitalController::class, 'index'])->where('di
 Route::get('{contact}', [ContactController::class, 'index'])->where('contact', 'contact|lien-he');
 Route::get('{news}', [PostController::class, 'index'])->where('news', 'news|tin-tuc');
 Route::get('{news}/{post:slug}', [PostController::class, 'detail'])->where('news', 'news|tin-tuc');
-Route::get('{chanel}', [ChanelController::class, 'index'])->where('chanel', 'mua-kenh-youtube');
+Route::get('mua-kenh-youtube', [ChanelController::class, 'index']);
 Route::post('customer', [CustomerController::class, 'store']);
 Route::post('cms', [CmsController::class, 'index']);
 
