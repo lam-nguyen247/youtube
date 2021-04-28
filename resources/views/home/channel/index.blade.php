05bb2556b6f9e4b7f8827139195fa63a73f69862
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-04-28 13:15:59 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -3,6 +3,11 @@
 @section('title', __('Channels'))
 @section('css')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.css">
+<script
+  src="https://code.jquery.com/jquery-3.6.0.min.js"
+  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous">
+</script>
 <style>
     .dataTables_length{
         margin-bottom: 15px;
@@ -62,10 +67,7 @@
     @include('home.includes.consultation')
 </div>
 @section("js")
-<script
-  src="https://code.jquery.com/jquery-3.6.0.min.js"
-  integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-  crossorigin="anonymous"></script>
+
 <script>
     $(document).ready( function () {
         $('#customers').DataTable({
