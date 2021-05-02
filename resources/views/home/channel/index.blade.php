f0935d940771e4b0690cad3386985e5127be8cec
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2021-05-02 21:51:48 +0700

    Update index.blade.php

--- resources/views/home/channel/index.blade.php
+++ resources/views/home/channel/index.blade.php
@@ -28,7 +28,7 @@
             </div>
             <div class="row" style="background-color:white; padding: 15px">
                 <div class="col-12 col-md-12 col-lg-12">
-                    <form action="/mua-kenh-youtube" method="GET">
+                    <form id="ds" action="/mua-kenh-youtube" method="GET">
                         @csrf
                         <div class="container">
                             <div class="row" style="margin-bottom: 20px;">
@@ -45,9 +45,9 @@
                                     <input onchange="MaximumSValidate()"  type="number" id="sub_t" name="sub_t">
                                 </div>
                                 <div class="col-12" style="margin-top:10px; text-align: right;">
-                                    <button type="reset" style="background-color: #E1CE69; border: 1px solid #E1CE69; margin-right: 10px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
+                                    <button onclick="search($event)" type="reset" style="background-color: #E1CE69; border: 1px solid #E1CE69; margin-right: 10px;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                     0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;">Hủy bỏ</button>
-                                     <button type="submit" style="background-color: #8CE78C; border: 1px solid #8CE78C;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
+                                     <button onsubmit="search($event)" type="submit" style="background-color: #8CE78C; border: 1px solid #8CE78C;  box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%),
                                      0 1px 18px 0 rgb(0 0 0 / 12%); padding: 5px 10px;" >Lọc</button>
                                 </div>
                             </div>
@@ -108,31 +108,23 @@
         if (parseInt(min) > parseInt(max)) {
             alert("Giá bắt đầu phải nhỏ hơn giá kết thúc.");
         }
-        console.log(min,max);
     }
 
-    function MaximumNValidate() {
+    function search(e){
         var min = document.getElementById("price_f").value;
         var max = document.getElementById("price_t").value;
         if (parseInt(max ) < parseInt(min)) {
             alert("Giá kết thúc phải lớn hơn giá bắt đầu.");
-        }
-        console.log(min,max);
-    }
-
-    function MinimumSValidate() {
-        var min = document.getElementById("sub_f").value;
-        var max = document.getElementById("sub_t").value;
-        if (parseInt(min) > parseInt(max)) {
-            alert("Lượng subscribers bắt đầu phải nhỏ hơn kết thúc.");
-        }
-    }
-
-    function MaximumSValidate() {
-        var min = document.getElementById("sub_f").value;
-        var max = document.getElementById("sub_t").value;
-        if (parseInt(max ) < parseInt(min)) {
-            alert("Lượng subscribers kết thúc phải lớn hơn bắt đầu.");
+            e.preventDefault();
+        }else{
+            min = document.getElementById("sub_f").value;
+            max = document.getElementById("sub_t").value;
+            if (parseInt(min) > parseInt(max)) {
+                alert("Lượng subscribers bắt đầu phải nhỏ hơn kết thúc.");
+                e.preventDefault();
+            }else{
+                $( "#ds" ).submit();
+            }
         }
     }
     function xemKenh(url){
