4de034f800b2672b462f59f112db8b0f2d04da31
Nguyễn Thành Lâm <lnguyen24794@gmail.com>
2022-03-28 08:33:47 +0700

    Update HomeController.php

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -9,6 +9,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        dd(1);
         $agent = new Agent();
         return view('home.index', compact('agent'));
     }
